598a0cc900c045505832060f520dd694b3c9fe29
tianma8023 <1808931265@qq.com>
2019-02-04 21:32:25 +0800

    [Optimize] Optimize the algorithm of parsing SMS Code.

--- app/src/main/java/com/github/tianma8023/smscode/utils/SmsCodeUtils.java
+++ app/src/main/java/com/github/tianma8023/smscode/utils/SmsCodeUtils.java
@@ -97,7 +97,7 @@ public class SmsCodeUtils {
         // 之前的正则表达式是 [a-zA-Z0-9]{4,8}
         // 现在的正则表达式是 [a-zA-Z0-9]+(\.[a-zA-Z0-9]+)? 匹配数字和字母之间最多一个.的字符串
         // 之前的不能识别和剔除小数，比如 123456.231，很容易就把 123456 作为验证码。
-        String codeRegex = "[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)?";
+        String codeRegex = "(?<![a-zA-Z0-9])[a-zA-Z0-9]{4,8}(?![a-zA-Z0-9])";
         return getSmsCode(codeRegex, keywordsRegex, content);
     }
 
@@ -132,7 +132,7 @@ public class SmsCodeUtils {
         int beginIndex = 0, endIndex = content.length() - 1;
         int curIndex = content.indexOf(matchedStr);
         int strLength = matchedStr.length();
-        int magicNumber = 14;
+        int magicNumber = 30;
         if (curIndex - magicNumber > 0) {
             beginIndex = curIndex - magicNumber;
         }
@@ -149,7 +149,7 @@ public class SmsCodeUtils {
         // 之前的正则表达式是 [0-9]{4,8} 匹配由数字组成的4到8长度的字符串
         // 现在的正则表达式是 [0-9]+(\\.[0-9]+)? 匹配数字之间最多一个.的字符串
         // 之前的不能识别和剔除小数，比如 123456.231，很容易就把 123456 作为验证码。
-        String codeRegex = "[0-9]+(\\.[0-9]+)?";
+        String codeRegex = "(?<![0-9])[0-9]{4,8}(?![0-9])";
         return getSmsCode(codeRegex, keywordsRegex, content);
     }
 
@@ -167,9 +167,7 @@ public class SmsCodeUtils {
         List<String> possibleCodes = new ArrayList<>();
         while (m.find()) {
             final String matchedStr = m.group();
-            if (matchedStr.length() >= 4 && matchedStr.length() <= 8 && !matchedStr.contains(".")) {
-                possibleCodes.add(matchedStr);
-            }
+            possibleCodes.add(matchedStr);
         }
         if (possibleCodes.isEmpty()) { // no possible code
             return "";
